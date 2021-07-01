ff0353df75a069a5fb6856396a9356a3482cc5d3
kanakana1003 <kanako.k.1003@gmail.com>
2021-07-01 21:25:41 +0900

    16行目以下削除

--- count.py
+++ count.py
@@ -12,18 +12,3 @@ for index, row in all_list.iterrows():
     count = count + row[5].count(search)
 
 print("全部で" + str(count) + "個")
-
-"""
-search = input("好きな単語を入力:")
-print("あなたが調べたいのは" + search)
-
-import pandas as pd
-from pandas.io.parsers import count_empty_vals
-
-all_list = pd.read_csv("./csv/sample.csv")
-
-for index, row in all_list.iterrows():
-    if search in row[5]:
-        print(row[1],row[5].count(search))
-"""
-
