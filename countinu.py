1baa4d1e96a416d879e6fb08d326172abb785811
h.kasamatsu <you.in729@gmail.com>
2021-07-01 21:36:29 +0900

    countinu.pyを追加する

---
+++ countinu.py
@@ -0,0 +1,13 @@
+search1 = input("好きな単語1:")
+search2 = input("好きな単語2:")
+
+print("あなたが調べたいのは" + search1 + search2)
+
+import pandas as pd
+from pandas.io.parsers import count_empty_vals
+
+all_list = pd.read_csv("./csv/sample.csv")
+
+for index, row in all_list.iterrows():
+    if search1 in row[5] and search2 in row[5]:
+        print(row[1],search1 + "の数は" + str(row[5].count(search1)),search2 + "の数は" + str(row[5].count(search2)))
