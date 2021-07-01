ff4d43dfb478b187a9e24f49af2fa7dec09153ea
inub <>
2021-07-01 21:29:43 +0900

    no message

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+
 search1 = input("好きな単語1:")
 search2 = input("好きな単語2:")
 
@@ -19,14 +20,10 @@ for index, row in all_list.iterrows():
 """
 search = input("好きな単語を入力:")
 print("あなたが調べたいのは" + search)
-
 import pandas as pd
 from pandas.io.parsers import count_empty_vals
-
 all_list = pd.read_csv("./csv/sample.csv")
-
 for index, row in all_list.iterrows():
     if search in row[5]:
         print(row[1],row[5].count(search))
 """
-
